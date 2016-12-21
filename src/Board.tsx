084892c09ca1acf16d3ab738fbec0d6b26b7cadc
Evgeniy Khramkov <ewgeniux@gmail.com>
2016-12-21 18:14:24 +0300

    upd

--- src/Board.tsx
+++ src/Board.tsx
@@ -72,13 +72,48 @@ export default class Board extends Component<BoardProps, BoardState> {
     const cell = cells[index]
     if (cell0 === -1) {
       this.setState({
-        cell0: index
+        cell0: index,
+        cell1: -1
       })
     } else {
       const coords0 = this.getCoords(cell0)
       if (coords0.i === i && coords0.j === j) {
         this.setState({
-          cell0: -1
+          cell0: -1,
+          cell1: -1
+        })
+      } else if (
+        (coords0.i === i && Math.abs(coords0.j - j) === 1) ||
+        (coords0.j === j && Math.abs(coords0.i - i) === 1)
+      ) {
+        const i0 = Math.min(cell0, index)
+        const i1 = Math.max(cell0, index)
+        const cellItem0 = cells[i0]
+        const cellItem1 = cells[i1]
+        this.setState({
+          cell0: -1,
+          cell1: -1,
+          cells: [
+            ...cells.slice(0, i0),
+            {
+              ...cellItem0,
+              value: cellItem1.value
+            },
+            ...cells.slice(i0 + 1, i1),
+            {
+              ...cellItem1,
+              value: cellItem0.value
+            },
+            ...cells.slice(i1 + 1)
+          ]
+        })
+        /*this.setState({
+          cell1: index
+        })*/
+      } else {
+        this.setState({
+          cell0: index,
+          cell1: -1
         })
       }
     }
